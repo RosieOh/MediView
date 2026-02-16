6377dd06032cc4088d8602487dbd4fa62ae2edc4
RosieOh <dhxogns920@gmail.com>
2026-02-16 15:07:22 +0900

    FEAT: 알림 레포지토리 NotificationRepository 추가

---
+++ src/main/java/com/mediview/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,13 @@
+package com.mediview.domain.notification.repository;
+
+import com.mediview.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
+
+    List<Notification> findByUserIdAndIsReadFalse(Long userId);
+}
