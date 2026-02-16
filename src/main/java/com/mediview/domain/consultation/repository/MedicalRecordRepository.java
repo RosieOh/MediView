bdc3be38c9bf439d56e19a0cae6db1ec81997799
RosieOh <dhxogns920@gmail.com>
2026-02-16 15:07:22 +0900

    FEAT: 의료 기록 레포지토리 MedicalRecordRepository 추가

---
+++ src/main/java/com/mediview/domain/consultation/repository/MedicalRecordRepository.java
@@ -0,0 +1,11 @@
+package com.mediview.domain.consultation.repository;
+
+import com.mediview.domain.consultation.entity.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
+
+    Optional<MedicalRecord> findByAppointmentId(Long appointmentId);
+}
