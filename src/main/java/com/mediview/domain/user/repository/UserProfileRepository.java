01fafd736e130815bd5836face78b29b30e7a907
RosieOh <dhxogns920@gmail.com>
2026-02-16 15:07:21 +0900

    FEAT: 사용자 프로필 레포지토리 UserProfileRepository 추가

---
+++ src/main/java/com/mediview/domain/user/repository/UserProfileRepository.java
@@ -0,0 +1,11 @@
+package com.mediview.domain.user.repository;
+
+import com.mediview.domain.user.entity.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+
+    Optional<UserProfile> findByUserId(Long userId);
+}
