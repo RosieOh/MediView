c63fcf71f32ee6b8c1161ba6c8fde36b89774283
RosieOh <dhxogns920@gmail.com>
2026-02-16 15:07:20 +0900

    FEAT: 진료 과목(전문 분야) 열거형 추가

---
+++ src/main/java/com/mediview/domain/enums/Specialization.java
@@ -0,0 +1,13 @@
+package com.mediview.domain.enums;
+
+public enum Specialization {
+    GENERAL_PRACTICE,
+    PEDIATRICS,
+    CARDIOLOGY,
+    DERMATOLOGY,
+    NEUROLOGY,
+    PSYCHIATRY,
+    DENTISTRY,
+    OPHTHALMOLOGY,
+    EMERGENCY_MEDICINE
+}
