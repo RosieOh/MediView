373d83822e90a582475af71f9a9ef426c0b2a7ba
RosieOh <dhxogns920@gmail.com>
2026-02-16 15:07:21 +0900

    FEAT: 사용자 레포지토리 UserRepository 추가

---
+++ src/main/java/com/mediview/domain/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.mediview.domain.user.repository;
+
+import com.mediview.domain.enums.UserRole;
+import com.mediview.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    List<User> findByRole(UserRole role);
+}
